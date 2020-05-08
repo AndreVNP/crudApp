c4f96fc38d0448f415313325e070b805d05bce6d
André Vicente <andre.vnp@live.com>
2020-05-08 17:52:41 -0300

    Configurando conexão com banco de dados não relacional

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCUZeIC8zeOJvZHcqh8OSgdPK7xXC8-hO8",
+    authDomain: "crudappandrevnp.firebaseapp.com",
+    databaseURL: "https://crudappandrevnp.firebaseio.com",
+    projectId: "crudappandrevnp",
+    storageBucket: "crudappandrevnp.appspot.com",
+    messagingSenderId: "83467294631",
+    appId: "1:83467294631:web:cf6ce857fef259465f0882",
+    measurementId: "G-JZ1M4PKTN6"
+  }
 };
